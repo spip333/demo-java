65a406212754851c3af2d7d119097e70fd263ed5
Stern Nicolas (U115136) <@mobi.ch>
2020-09-25 15:13:33 +0200

    demo pattern matching

--- src/tests/java/org/nstern/demos/app/StringPatternValidationDemoTest.java
+++ src/tests/java/org/nstern/demos/app/StringPatternValidationDemoTest.java
@@ -12,12 +12,22 @@ class StringPatternValidationDemoTest {
 
     @Test
     void testPatternMatches() {
-        Pattern p = compile("\\d\\d\\d");
+        Pattern p1 = compile("\\d\\d\\d");
         String s1 = "111";
         String s2 = "aaa";
 
-        assertTrue(StringPatternValidationDemo.check(p, s1));
-        assertFalse(StringPatternValidationDemo.check(p, s2));
+        assertTrue(StringPatternValidationDemo.check(p1, s1));
+        assertFalse(StringPatternValidationDemo.check(p1, s2));
+
+        Pattern p2 = compile("\\d+");
+
+        assertTrue(StringPatternValidationDemo.check(p2, s1));
+        assertFalse(StringPatternValidationDemo.check(p2, s2));
+
+        Pattern p3 = compile("\\d+");
+
+        assertTrue(StringPatternValidationDemo.check(p2, s1));
+        assertFalse(StringPatternValidationDemo.check(p2, s2));
     }
 
 }
