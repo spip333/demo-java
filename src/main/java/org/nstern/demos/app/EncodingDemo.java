f050082dba453c096c015a7f199b4e5a2106038b
U115136 <U115136@mobi.ch>
2021-03-26 15:03:52 +0100

    added demo

---
+++ src/main/java/org/nstern/demos/app/EncodingDemo.java
@@ -0,0 +1,10 @@
+package org.nstern.demos.app;
+
+public class EncodingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("aeiou, äöü, àéèîôâ");
+
+    }
+
+}
---
+++ src/main/java/org/nstern/demos/app/EnumDemo.java
@@ -0,0 +1,17 @@
+package org.nstern.demos.app;
+
+public class EnumDemo {
+
+    public static void main(String[] args) {
+        String one = "one";
+
+        System.out.println("one".equals(Values.one.toString()));
+        System.out.println(Values.one.toString());
+        System.out.println(Values.values().toString());
+        System.out.println(EnumUtils.isValues.values().toString());
+
+
+    }
+}
+
+enum Values{one, two, three}
