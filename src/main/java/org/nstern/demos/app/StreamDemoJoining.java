59bfd04f159e6e915a5036bcf7379bbfc83e236a
Stern Nicolas(U115136) <nicolas.stern@mobi.ch>
2022-06-17 13:53:11 +0200

    added demos

--- src/main/java/org/nstern/demos/app/StreamDemoJoining.java
+++ src/main/java/org/nstern/demos/app/StreamDemoJoining.java
@@ -1,5 +1,6 @@
 package org.nstern.demos.app;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,17 @@ public class StreamDemoJoining {
 
     public static void main(String[] args) {
         Set<Integer> mySet = Set.of(1000, 1001, 1002, 1003);
-
-        String s1 = mySet.stream().map(String::valueOf).collect(Collectors.joining(","));
-
-        // code above is equivalent to
+        // code is equivalent to :
         // mySet.stream().map(el -> String.valueOf(el)).collect(Collectors.joining(","));
+        String s1 = mySet.stream().map(String::valueOf).collect(Collectors.joining(", "));
+       System.out.println(s1);
 
-        System.out.println(s1);
+        List<String> stringList = List.of("Pierre", "Jacques", "Jean");
+        // code is equivalent to :
+        // String.join(", ", stringList)
+        String s2 = stringList.stream().collect(Collectors.joining(", "));
+        System.out.println(s2);
+        String s3 = String.join(", ", stringList);
+        System.out.println(s3);
     }
 }
