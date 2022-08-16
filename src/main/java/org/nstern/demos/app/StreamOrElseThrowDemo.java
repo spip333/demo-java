a26fb76c317c37bcb5c1468cf078d2a9134869df
Stern Nicolas(U115136) <nicolas.stern@mobi.ch>
2022-08-16 13:33:35 +0200

    added demos

---
+++ src/main/java/org/nstern/demos/app/StreamOrElseThrowDemo.java
@@ -0,0 +1,44 @@
+package org.nstern.demos.app;
+
+import static org.nstern.demos.util.H.p;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.nstern.demos.dto.Car;
+
+
+public class StreamOrElseThrowDemo {
+
+    public static void main(String[] args) {
+        StreamOrElseThrowDemo main = new StreamOrElseThrowDemo();
+
+        List<Car> carListEmpty = Collections.emptyList();
+        List<Car> carListNotEmpty = List.of(
+                new Car.Builder().withMark("ford").withModel("mustang").build(),
+                new Car.Builder().withMark("ford").withModel("cmax").build());
+
+        p("list not empty ...");
+
+        try {
+            p(main.doIt(carListNotEmpty));
+        } catch (Exception e) {
+            p("" + e.getMessage());
+        }
+
+        p("list empty ...");
+
+        try {
+            p(main.doIt(carListEmpty));
+        } catch (Exception e) {
+            p("" + e);
+        }
+
+    }
+
+    public String doIt(List<Car> carList) {
+        carList.stream().findFirst().orElseThrow(RuntimeException::new);
+        return "no exception found for " + carList;
+    }
+
+}
